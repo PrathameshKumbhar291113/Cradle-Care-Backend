e749bf884465e4bb1c08755e1ea5d6c1202e59e2
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-02-16 14:12:50 +0530

    added the jdbc url

--- src/main/kotlin/com/cradlecare/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/cradlecare/repository/DatabaseFactory.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -23,10 +24,17 @@ object DatabaseFactory {
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl = System.getenv("CRADLE_CARE_DATABASE_URL")
+//        config.jdbcUrl = System.getenv("CRADLE_CARE_DATABASE_URL")
         config.maximumPoolSize = 3
         config.isAutoCommit = true
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val userName = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl = "jdbc:postgresql://"+uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$userName&password=$password"
+
         config.validate()
         return HikariDataSource(config)
     }
